86a537d59a349de044f54053b23f9e9f2c63a54d
MUdeveloper <167856361+MUdeveloper@users.noreply.github.com>
2025-08-01 17:38:24 +0530

    Trigger redeploy

--- db.py
+++ db.py
@@ -2,14 +2,7 @@ from mysql.connector import (connection)
 import os
 from dotenv import load_dotenv
 load_dotenv()
-print("function work start")
-print("trying to connect to database")
-print("Trying to connect to DB with values:")
-print("User:", os.getenv("MYSQL_USER"))
-print("Password:", os.getenv("MYSQL_PASSWORD"))
-print("Host:", os.getenv("MYSQL_HOST"))
-print("Port:", os.getenv("MYSQL_PORT"))
-print("Database:", os.getenv("MYSQL_DB"))
+
 def db_connection():
     return connection.MySQLConnection(
     user=os.getenv("MYSQL_USER"),
